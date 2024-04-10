c59e9d753f53ba9e72f9b589b665b001256c7873
Razvan Pop <pop.razvan03@yahoo.com>
2024-04-10 20:54:15 +0300

    added a way to export data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const Card = ({id, name, email}) => {
+const Card = ({id, name, email, checkboxChange}) => {
   return(
-    <div className='bg-light-green dib br3 pa3 ma2 grow bw shadow-5'>
+    <div className='bg-light-green dib br3 pa3 ma2 bw shadow-5'>
+      <input
+        type='checkbox'
+        onChange={() => checkboxChange(id)}
+      />
       <img alt='robots' width='200' height='200' src={`https://robohash.org/${id}?size=200x200`} />
         <div>
           <h2>{name}</h2>
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Card from './Card'
 
-const CardList = ({robots}) => {
+const CardList = ({robots, checkboxChange}) => {
   return(
     <div>
       {
         robots.map((user, index) => {
           return (
             <Card 
-              key={index}
               id={robots[index].id}
               name={robots[index].name}
               email={robots[index].email}
+              key={robots[index].id}
+              checkboxChange={checkboxChange}
             />
           );
         })
