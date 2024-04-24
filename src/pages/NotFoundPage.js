802d1e7d3569df486764fff98caa5f0aaa1f26a4
Razvan Pop <pop.razvan03@yahoo.com>
2024-04-25 02:19:28 +0300

    Add NotFoundPage

---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  <>
+    <h1>404: Not Found</h1>
+    <p>Sorry, the page you are looking for does not exist.</p>
+  </>
+}
+
+export default NotFoundPage;
