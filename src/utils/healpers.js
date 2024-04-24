31765c11ed149552234746e4b0f1d1e77e390d9b
Razvan Pop <pop.razvan03@yahoo.com>
2024-04-25 02:07:54 +0300

    Add utils

---
+++ src/utils/healpers.js
@@ -0,0 +1,8 @@
+export const filterArray = (array, query) => {
+  return array.filter(element => element.name.includes(query).toLowerCase());
+};
+
+export const isValidEmail = (email) => {
+  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(email);
+};
