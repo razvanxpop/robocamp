532362cd05d6ccee9e898f5179a454bdbaf2e847
Razvan Pop <pop.razvan03@yahoo.com>
2024-04-25 02:19:10 +0300

    Update ui elements

--- src/components/ui/Input.js
+++ src/components/ui/Input.js
@@ -1,7 +1,7 @@
 const Input = ({ type, placeholder, value, onChange }) => {
   return (
     <input
-      className="pa3 ba b--gold bg-gold ma2 bg-color-white"
+      className="pa3 ba b--gold bg-gold ma2"
       type={type}
       placeholder={placeholder}
       value={value}
--- src/components/ui/RobotImage.js
+++ src/components/ui/RobotImage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const RobotImage = ({id}) => {
-    return <img alt='robots' width='200' height='200' src={`https://robohash.org/${id}?size=200x200`} />
+const RobotImage = ({id, height, width}) => {
+    return <img alt='robots' width={width} height={height} src={`https://robohash.org/${id}?size=200x200`} />
 }
 
 export default RobotImage;
